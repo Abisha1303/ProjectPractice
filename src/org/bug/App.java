5fe8eed4fe9800be42ef82489cbe225f7d86d4ac
Abisha1303 <abisha.kulfi1997@gmail.com>
2020-07-21 15:33:45 +0530

    code updated

--- src/org/bug/App.java
+++ src/org/bug/App.java
@@ -2,4 +2,18 @@ package org.bug;
 
 public class App {
 
+	public void photos() {
+		System.out.println("check for Google photos");
+	}
+	
+	
+	public void map() {
+		System.out.println("check for Google map");
+	}
+	public void googlePlus() {
+		System.out.println("check for Google plus");
+	}
+	
+	
+	
 }
