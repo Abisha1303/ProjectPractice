ea41e56329c2c589491381860d0b84d08b6e6b2c
Abisha1303 <abisha.kulfi1997@gmail.com>
2020-07-22 10:56:26 +0530

    conflicts

--- src/org/bug/App.java
+++ src/org/bug/App.java
@@ -1,7 +1,7 @@
 package org.bug;
 
 public class App {
-<<<<<<< HEAD
+
 
 	public void photos() {
 		System.out.println("check for Google photos");
@@ -14,9 +14,9 @@ public class App {
 	
 	public void gmail() {
 		System.out.println("login to ur mail");
-=======
+	}
 	public static void main(String[] args) {
 		System.out.println("this is main method");
->>>>>>> d4b5bdbe9fa02269f409d3384efef57d8e04df4c
+
 	}
 }
